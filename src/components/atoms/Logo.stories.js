e91bf18f0cb64f40f594d8f9fa369e880ceabe35
Karol <karolprzybylak1@gmail.com>
2021-03-17 23:53:36 +0100

    Added storybook and atomic design #1

---
+++ src/components/atoms/Logo.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {Logo} from './Logo';
+
+export default {
+    title: 'Logo',
+    component: Logo
+};
+
+const Template = () => <Logo>TEST </Logo>;
+
+export const LogoExample = Template.bind({});
