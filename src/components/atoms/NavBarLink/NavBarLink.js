785478b0b65a57be29f824dae4b40634767f5866
Karol <karolprzybylak1@gmail.com>
2021-03-24 22:10:55 +0100

    Added nav bar link

---
+++ src/components/atoms/NavBarLink/NavBarLink.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const NavBarLink = styled.a`
+    font-size: 1.2em;
+  color: black;
+  font-weight: bold;
+  padding: 0 10px;
+`
+
+export default NavBarLink
